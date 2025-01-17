69055b4dcb528211039bee19c4bf1f449b4f9a42
Renuka Pangam <159953063+renukapangamgithub@users.noreply.github.com>
2025-01-17 23:25:12 +0530

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -21,7 +21,7 @@ const Login = () => {
     }
 
     try {
-      const response = await axios.post('http://localhost:4000/api/auth/login', { email, password });
+      const response = await axios.post('https://decosphere-backend1.onrender.com/api/auth/login', { email, password });
 
       console.log('Login Response:', response); 
 
